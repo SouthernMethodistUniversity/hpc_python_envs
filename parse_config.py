ac78d41291c18fa1c79c7fb03432c5709ec5e228
John LaGrone <jlagrone@gmail.com>
2025-01-09 15:39:26 -0600

    add script to parse confics

---
+++ parse_config.py
@@ -0,0 +1,20 @@
+import json
+import argparse
+import pathlib
+
+if __name__ == "__main__":
+
+    # Set up command line options
+    parser = argparse.ArgumentParser(description='Parse config file for building python env')
+    parser.add_argument('input', nargs=1, type=pathlib.Path)
+    parser.add_argument('--versions', help="print pyhton version(s) to install", default=False, action='store_true')
+
+    # parse the input
+    args = parser.parse_args()
+    print(args)
+
+    with open(args.input[0], 'r') as file:
+        data = json.load(file)
+
+        if args.versions:
+            print(','.join(data['python versions']))
