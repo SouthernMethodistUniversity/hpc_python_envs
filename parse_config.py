7b7efc121514be26d027f43af37888bf8d733b5d
John LaGrone <jlagrone@gmail.com>
2025-01-09 15:44:04 -0600

    add description

--- parse_config.py
+++ parse_config.py
@@ -8,13 +8,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse config file for building python env')
     parser.add_argument('input', nargs=1, type=pathlib.Path)
     parser.add_argument('--versions', help="print pyhton version(s) to install", default=False, action='store_true')
+    parser.add_argument('--description', help="print pyhton version(s) to install", default=False, action='store_true')
 
     # parse the input
     args = parser.parse_args()
-    print(args)
 
     with open(args.input[0], 'r') as file:
         data = json.load(file)
 
         if args.versions:
             print(','.join(data['python versions']))
+        elif args.description:
+           print(data['description'])
