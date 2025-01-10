104c962858ff9d0aa656db3085aadb1407a2a279
John LaGrone <jlagrone@gmail.com>
2025-01-10 11:09:28 -0600

    don't use conda /mamba command

--- activate_env.sh
+++ activate_env.sh
@@ -5,8 +5,8 @@ ENV_ACTIVATE=${ENV_PATH}/bin/activate
 if [ -f $ENV_ACTIVATE ]; then
    . ${ENV_ACTIVATE}
 else
-   mamba activate $1
-   python -m venv --system-site-packages --symlinks ${ENV_PATH}
-   mamba deactivate
+   #mamba activate $1
+   ${1}/bin/python -m venv --system-site-packages --symlinks ${ENV_PATH}
+   #mamba deactivate
    . ${ENV_ACTIVATE}
 fi
