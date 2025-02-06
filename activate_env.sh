df02f81c2640ca5e9bdf294f82b301cd33238b73
John LaGrone <jlagrone@gmail.com>
2025-02-06 15:13:08 -0600

    activate conda env

--- activate_env.sh
+++ activate_env.sh
@@ -3,10 +3,10 @@
 ENV_PATH=$2
 ENV_ACTIVATE=${ENV_PATH}/bin/activate
 if [ -f $ENV_ACTIVATE ]; then
+   mamba activate $1
    . ${ENV_ACTIVATE}
 else
-   #mamba activate $1
+   mamba activate $1
    ${1}/bin/python -m venv --system-site-packages --symlinks ${ENV_PATH}
-   #mamba deactivate
    . ${ENV_ACTIVATE}
 fi
