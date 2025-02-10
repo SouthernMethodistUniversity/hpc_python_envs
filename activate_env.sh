b8ea74a235e960420a44f929fc3539ec5bb7fbb9
John LaGrone <jlagrone@gmail.com>
2025-02-10 13:40:29 -0600

    always activate conda

--- activate_env.sh
+++ activate_env.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+CONDA_INIT=$3
 ENV_PATH=$2
 ENV_ACTIVATE=${ENV_PATH}/bin/activate
+
+# load conda env
+source ${CONDA_INIT}
+conda activate ${1}
+
 if [ -f $ENV_ACTIVATE ]; then
-   mamba activate $1
    . ${ENV_ACTIVATE}
 else
-   mamba activate $1
    ${1}/bin/python -m venv --system-site-packages --symlinks ${ENV_PATH}
    . ${ENV_ACTIVATE}
 fi
